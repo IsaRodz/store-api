536d7a5e9b7039f7ea8b3e09f046e3435a107dd6
IsaRodz <rodrigueziriarte96@gmail.com>
2021-05-15 16:37:41 -0500

    Improved product controller

--- src/controllers/product.controller.js
+++ src/controllers/product.controller.js
@@ -1,10 +1,56 @@
 const connection = require('../config/database');
 
 class ProductController {
-    getAllProducts(req, res) {
-        connection.query('SELECT * FROM product', (error, result) => {
+    search(req, res) {
+        const { name, category, minPrice, maxPrice } = req.query;
+        const requestHandler = (error, result) => {
+            if (error) {
+                console.log({ error });
+                return res.status(500).send('Something went wrong');
+            }
+
             return res.json(result);
-        });
+        };
+
+        let sql = `SELECT * FROM product WHERE name LIKE '%${name}%'`;
+
+        if (category) {
+            sql += ` AND category = ${category}`;
+        }
+
+        if (minPrice) {
+            sql += ` AND price > ${minPrice}`;
+        }
+
+        if (maxPrice) {
+            sql += ` AND price < ${maxPrice}`;
+        }
+
+        connection.query(sql, requestHandler);
+    }
+
+    getById(req, res) {
+        const { id } = req.params;
+        const sql = `
+            SELECT 
+                product.id, 
+                product.name as name, 
+                product.url_image, 
+                product.price, 
+                product.discount,
+                category.name as category
+            FROM product INNER JOIN category ON product.category=category.id 
+            WHERE product.id = ${id}`;
+
+        const requestHandler = (error, result) => {
+            if (error) {
+                console.log(error);
+                return res.status(500).send('Something went wrong');
+            }
+
+            return res.json(result[0]);
+        };
+        connection.query(sql, requestHandler);
     }
 }
 
--- src/routes/products.js
+++ src/routes/products.js
@@ -2,6 +2,7 @@ const { Router } = require('express');
 const router = Router();
 const productController = require('../controllers/product.controller');
 
-router.route('/').get(productController.getAllProducts);
+router.route('/').get(productController.search);
+router.route('/:id').get(productController.getById);
 
 module.exports = router;
